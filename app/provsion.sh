62a8c6ba20812069f83e71b848643d675a2d4280
Samuel Naiwo <samuelnaiwo@Samuels-MacBook-Pro.local>
2023-04-19 18:08:08 +0100

    update

--- app/provsion.sh
+++ app/provsion.sh
@@ -3,13 +3,12 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install nginx -y
-sudo systemctl start nginx
-sudo systemctl enable
-sudo apt-get install python -y
-# install nodejs
-sudo apt-get install python-software-properties
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo systemctl enable nginx
+sudo systemctl restart nginx
+sudo apt-get install python-software-properties -y
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs -y
-
-# install pm2
 sudo npm install pm2 -g
+
+npm install
+node app.js
